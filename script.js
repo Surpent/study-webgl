535bb650924142bcc8002b2234b85309afb37c33
surpent <surpenter@gmail.com>
2017-01-20 16:25:49 +0900

    トーラスを描画

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
-// sample_007
+// sample_008
 //
-// WebGLでカリングと深度テストを有効にする
+// WebGLで立体的なトーラスを描画する
 
 onload = function(){
 	// canvasエレメントを取得
 	var c = document.getElementById('canvas');
 	c.width = 500;
 	c.height = 300;
-	
-	// チェックボックスの参照を取得
-	var che_culling = document.getElementById('cull');
-	var che_front = document.getElementById('front');
-	var che_depth_test = document.getElementById('depth');
 	
 	// webglコンテキストを取得
 	var gl = c.getContext('webgl') || c.getContext('experimental-webgl');
@@ -24,34 +19,20 @@ onload = function(){
 	var prg = create_program(v_shader, f_shader);
 	
 	// attributeLocationを配列に取得
-	var attLocation = new Array(2);
+	var attLocation = new Array();
 	attLocation[0] = gl.getAttribLocation(prg, 'position');
 	attLocation[1] = gl.getAttribLocation(prg, 'color');
 	
 	// attributeの要素数を配列に格納
-	var attStride = new Array(2);
+	var attStride = new Array();
 	attStride[0] = 3;
 	attStride[1] = 4;
 	
-	// 頂点属性を格納する配列
-	var position = [
-		 0.0,  1.0,  0.0,
-		 1.0,  0.0,  0.0,
-		-1.0,  0.0,  0.0,
-		 0.0, -1.0,  0.0
-	];
-	var color = [
-		1.0, 0.0, 0.0, 1.0,
-		0.0, 1.0, 0.0, 1.0,
-		0.0, 0.0, 1.0, 1.0,
-		1.0, 1.0, 1.0, 1.0
-	];
-	
-	// 頂点のインデックスを格納する配列
-	var index = [
-		0, 1, 2,
-		1, 2, 3
-	];
+	// トーラスの頂点データを生成
+	var torusData = torus(32, 32, 1.0, 2.0);
+	var position = torusData[0];
+	var color = torusData[1];
+	var index = torusData[2];
 	
 	// VBOの生成
 	var pos_vbo = create_vbo(position);
@@ -81,35 +62,20 @@ onload = function(){
 	var mvpMatrix = m.identity(m.create());
 	
 	// ビュー×プロジェクション座標変換行列
-	m.lookAt([0.0, 0.0, 5.0], [0, 0, 0], [0, 1, 0], vMatrix);
+	m.lookAt([0.0, 0.0, 20.0], [0, 0, 0], [0, 1, 0], vMatrix);
 	m.perspective(45, c.width / c.height, 0.1, 100, pMatrix);
 	m.multiply(pMatrix, vMatrix, tmpMatrix);
 	
 	// カウンタの宣言
 	var count = 0;
 	
-	// 深度テストの比較方法を指定
+	// カリングと深度テストを有効にする
+	gl.enable(gl.DEPTH_TEST);
 	gl.depthFunc(gl.LEQUAL);
+	gl.enable(gl.CULL_FACE);
 	
 	// 恒常ループ
 	(function(){
-		// checkBoxの値によってカリングと深度テストを設定
-		if(che_culling.checked){
-			gl.enable(gl.CULL_FACE);
-		}else{
-			gl.disable(gl.CULL_FACE);
-		}
-		if(che_front.checked){
-			gl.frontFace(gl.CCW);
-		}else{
-			gl.frontFace(gl.CW);
-		}
-		if(che_depth_test.checked){
-			gl.enable(gl.DEPTH_TEST);
-		}else{
-			gl.disable(gl.DEPTH_TEST);
-		}
-		
 		// canvasを初期化
 		gl.clearColor(0.0, 0.0, 0.0, 1.0);
 		gl.clearDepth(1.0);
@@ -120,21 +86,10 @@ onload = function(){
 		
 		// カウンタを元にラジアンを算出
 		var rad = (count % 360) * Math.PI / 180;
-		var x = Math.cos(rad) * 1.5;
-		var z = Math.sin(rad) * 1.5;
 		
-		// モデル座標変換行列の生成(X軸による回転)
+		// モデル座標変換行列の生成
 		m.identity(mMatrix);
-		m.translate(mMatrix, [x, 0.0, z], mMatrix);
-		m.rotate(mMatrix, rad, [1, 0, 0], mMatrix);
-		m.multiply(tmpMatrix, mMatrix, mvpMatrix);
-		gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
-		gl.drawElements(gl.TRIANGLES, index.length, gl.UNSIGNED_SHORT, 0);
-		
-		// モデル座標変換行列の生成(Y軸による回転)
-		m.identity(mMatrix);
-		m.translate(mMatrix, [-x, 0.0, -z], mMatrix);
-		m.rotate(mMatrix, rad, [0, 1, 0], mMatrix);
+		m.rotate(mMatrix, rad, [0, 1, 1], mMatrix);
 		m.multiply(tmpMatrix, mMatrix, mvpMatrix);
 		gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
 		gl.drawElements(gl.TRIANGLES, index.length, gl.UNSIGNED_SHORT, 0);
@@ -268,5 +223,51 @@ onload = function(){
 		// 生成したIBOを返して終了
 		return ibo;
 	}
-
+	
+	function torus(row, column, irad, orad){
+		var pos = new Array(), col = new Array(), idx = new Array();
+		for(var i = 0; i <= row; i++){
+			var r = Math.PI * 2 / row * i;
+			var rr = Math.cos(r);
+			var ry = Math.sin(r);
+			for(var ii = 0; ii <= column; ii++){
+				var tr = Math.PI * 2 / column * ii;
+				var tx = (rr * irad + orad) * Math.cos(tr);
+				var ty = ry * irad;
+				var tz = (rr * irad + orad) * Math.sin(tr);
+				pos.push(tx, ty, tz);
+				var tc = hsva(360 / column * ii, 1, 1, 1);
+				col.push(tc[0], tc[1], tc[2], tc[3]);
+			}
+		}
+		for(i = 0; i < row; i++){
+			for(ii = 0; ii < column; ii++){
+				r = (column + 1) * i + ii;
+				idx.push(r, r + column + 1, r + 1);
+				idx.push(r + column + 1, r + column + 2, r + 1);
+			}
+		}
+		return [pos, col, idx];
+	}
+	
+	// HSVカラー取得用関数
+	function hsva(h, s, v, a){
+		if(s > 1 || v > 1 || a > 1){return;}
+		var th = h % 360;
+		var i = Math.floor(th / 60);
+		var f = th / 60 - i;
+		var m = v * (1 - s);
+		var n = v * (1 - s * f);
+		var k = v * (1 - s * (1 - f));
+		var color = new Array();
+		if(!s > 0 && !s < 0){
+			color.push(v, v, v, a); 
+		} else {
+			var r = new Array(v, n, m, m, k, v);
+			var g = new Array(k, v, v, n, m, m);
+			var b = new Array(m, m, k, v, v, n);
+			color.push(r[i], g[i], b[i], a);
+		}
+		return color;
+	}
 };
